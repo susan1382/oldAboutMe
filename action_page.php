1711b30dfee9c7666f2b103e17847b757a951036
susan1382 <106560768+susan1382@users.noreply.github.com>
2022-06-20 12:38:47 -0400

    added some files

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<pre style = "Font-size: 20px; text-align: center">
+  Answer Key:
+  1. Watching Youtube
+  2. Reading Webtoons
+  3. Calling My Friends
+  4. Playing Games
+  5. Talking
+
+  Did you get them correct?
+</pre>
